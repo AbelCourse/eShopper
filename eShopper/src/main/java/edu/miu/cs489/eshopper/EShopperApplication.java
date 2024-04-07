7387a6a938b3b87e270950c6e41939e533cfd0f0
AbelSeyoum <abelcourse12@gmail.com>
2024-04-07 02:13:26 -0500

    skeleton for project

---
+++ eShopper/src/main/java/edu/miu/cs489/eshopper/EShopperApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.eshopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShopperApplication.class, args);
+	}
+
+}
